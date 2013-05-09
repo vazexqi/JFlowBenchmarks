51b13d28d0c66ca9e29bf959d30e23136eb27e23
Nicholas Chen <nchen@illinois.edu>
2013-05-09 08:38:29 -0500

    Add newline to MonteCarlo run.sh to be consistent
    See #13.

--- MonteCarlo/run.sh
+++ MonteCarlo/run.sh
@@ -5,3 +5,4 @@ gradle jar
 java -Xms512M -Xmx512M -cp .:build/libs/MonteCarlo.jar:libs/gpars-1.1-jflow.jar:libs/groovy-all-2.1.3.jar:libs/jsr166y.jar edu.illinois.jflow.benchmark.JGFMonteCarloBench
 java -Xms512M -Xmx512M -cp .:build/libs/MonteCarlo.jar:libs/gpars-1.1-jflow.jar:libs/groovy-all-2.1.3.jar:libs/jsr166y.jar edu.illinois.jflow.benchmark.JGFMonteCarloBench
 java -Xms512M -Xmx512M -cp .:build/libs/MonteCarlo.jar:libs/gpars-1.1-jflow.jar:libs/groovy-all-2.1.3.jar:libs/jsr166y.jar edu.illinois.jflow.benchmark.JGFMonteCarloBench
+
