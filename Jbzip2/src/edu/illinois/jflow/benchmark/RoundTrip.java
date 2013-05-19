1a3dba30ddf1829c5355a7fd06de0c8dd1bf56aa
Nicholas Chen <nchen@illinois.edu>
2013-05-19 14:27:59 -0500

    Parallel version of Jbzip2

--- Jbzip2/src/edu/illinois/jflow/benchmark/RoundTrip.java
+++ Jbzip2/src/edu/illinois/jflow/benchmark/RoundTrip.java
@@ -1,5 +1,9 @@
 package edu.illinois.jflow.benchmark;
 
+import groovyx.gpars.DataflowMessagingRunnable;
+import groovyx.gpars.dataflow.DataflowQueue;
+import groovyx.gpars.dataflow.operator.FlowGraph;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -10,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.itadaki.bzip2.BZip2InputStream;
@@ -82,47 +87,78 @@ public class RoundTrip {
     }
 
 
+    static class Bundle {
+        File inputFile;
+
+        File uncompressedOutputFile;
+    }
+
+
     /**
      * Compresses and decompresses each of a list of files, using a temporary file to hold the
      * compressed data
      * 
      * @param files The files to compress
      * @throws IOException On any error compressing or decompressing the files
+     * @throws InterruptedException
      */
-    private static void roundTrip(List<File> files) throws IOException {
-        for (File inputFile : files) {
-            // Begin Stage1
-            File uncompressedOutputFile= File.createTempFile("jflow_in", ".tmp");
-            InputStream fileInputStream= new BufferedInputStream(new FileInputStream(inputFile));
-            BZip2InputStream compressedInputStream= new BZip2InputStream(fileInputStream, false);
-            OutputStream uncompressedFileStream= new BufferedOutputStream(new FileOutputStream(uncompressedOutputFile), 524288);
-
-            byte[] decoded= new byte[524288];
-            int bytesRead;
-            while ((bytesRead= compressedInputStream.read(decoded)) != -1) {
-                uncompressedFileStream.write(decoded, 0, bytesRead);
+    private static void roundTrip(List<File> files) throws IOException, InterruptedException {
+        final DataflowQueue<Bundle> channel0= new DataflowQueue<Bundle>();
+        final DataflowQueue<Bundle> channel1= new DataflowQueue<Bundle>();
+        FlowGraph fGraph= new FlowGraph();
+        fGraph.operator(Arrays.asList(channel0), Arrays.asList(channel1), 4, new DataflowMessagingRunnable(1) {
+            @Override
+            protected void doRun(Object... args) {
+                try {
+                    Bundle b= ((Bundle)args[0]);
+                    File inputFile= b.inputFile;
+                    File uncompressedOutputFile= File.createTempFile("jflow_in", ".tmp");
+                    InputStream fileInputStream= new BufferedInputStream(new FileInputStream(inputFile));
+                    BZip2InputStream compressedInputStream= new BZip2InputStream(fileInputStream, false);
+                    OutputStream uncompressedFileStream= new BufferedOutputStream(new FileOutputStream(uncompressedOutputFile), 524288);
+                    byte[] decoded= new byte[524288];
+                    int bytesRead;
+                    while ((bytesRead= compressedInputStream.read(decoded)) != -1) {
+                        uncompressedFileStream.write(decoded, 0, bytesRead);
+                    }
+                    uncompressedFileStream.close();
+                    compressedInputStream.close();
+                    b.uncompressedOutputFile= uncompressedOutputFile;
+                    channel1.bind(b);
+                } catch (Exception e) {
+                }
             }
-            uncompressedFileStream.close();
-            compressedInputStream.close();
-            // End Stage1
+        });
+        fGraph.operator(Arrays.asList(channel1), Arrays.asList(), 4, new DataflowMessagingRunnable(1) {
+            @Override
+            protected void doRun(Object... args) {
+                try {
+                    Bundle b= ((Bundle)args[0]);
+                    File uncompressedOutputFile= b.uncompressedOutputFile;
+                    File compressedOutputFile= File.createTempFile("jflow_out", ".bz2");
+                    InputStream fileStream= new BufferedInputStream(new FileInputStream(uncompressedOutputFile));
+                    OutputStream fileOutputStream= new BufferedOutputStream(new FileOutputStream(compressedOutputFile), 524288);
+                    BZip2OutputStream outputStream= new BZip2OutputStream(fileOutputStream);
+                    byte[] buffer= new byte[524288];
+                    int bytesWritten;
+                    while ((bytesWritten= fileStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesWritten);
+                    }
+                    outputStream.close();
+                    fileStream.close();
+                } catch (Exception e) {
+                }
+            }
+        });
+        for (File inputFile : files) {
+            Bundle b= new Bundle();
+            b.inputFile= inputFile;
+            channel0.bind(b);
 
             // Do some processing in the middle
 
-            // Begin Stage2
-            File compressedOutputFile= File.createTempFile("jflow_out", ".bz2");
-            InputStream fileStream= new BufferedInputStream(new FileInputStream(uncompressedOutputFile));
-            OutputStream fileOutputStream= new BufferedOutputStream(new FileOutputStream(compressedOutputFile), 524288);
-            BZip2OutputStream outputStream= new BZip2OutputStream(fileOutputStream);
-
-            byte[] buffer= new byte[524288];
-            int bytesWritten;
-            while ((bytesWritten= fileStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesWritten);
-            }
-            outputStream.close();
-            fileStream.close();
-            // End Stage2
         }
+        fGraph.waitForAll();
 
     }
 
@@ -130,8 +166,9 @@ public class RoundTrip {
     /**
      * @param args
      * @throws IOException
+     * @throws InterruptedException
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         if (args.length == 0) {
             System.err.println(
